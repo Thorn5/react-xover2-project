d0be16f0298ac5b8524ad8e8baf03d3d58ec240c
MirkoDuric <mirkodjuricczv@gmail.com>
2023-02-04 14:20:31 +0100

    Pulled test-Tabs branch

--- src/components/TabComponents/Location.js
+++ src/components/TabComponents/Location.js
@@ -1,11 +1,18 @@
-// import React, { useState, useEffect } from "react";
-// import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-// import useFetch from "../useFetch";
-// export default function Location() {
-//   const [zoom, setZoom] = useState(13);
-//   return <Map></Map>;
-// }
-
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import useFetch from "../useFetch";
 export default function Location() {
-  return <h3>Location Component</h3>;
+  const { restaurants } = useFetch();
+  const location = "location of our restaurant";
+  return (
+    <MapContainer center={location} zoom={12} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={location}>
+        <Popup>This is your location</Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
