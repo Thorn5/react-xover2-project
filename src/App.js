78bc773c2cf717460bf6da4ce61e738102eab361
Tryg <trygvehorn@gmail.com>
2023-02-04 10:59:09 +0000

    Update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import TabComponent from "./components/TabComponent";
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <TabComponent />
     </div>
   );
 }
