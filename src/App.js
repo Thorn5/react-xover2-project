3f3bf297869d27afa14c8bc4528d683917a6e5f5
Tryg <trygvehorn@gmail.com>
2023-02-04 11:38:18 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import "./App.css";
+import BasicTabs from "./components/TabComponent";
 
 function App() {
   return (
     <div className="App">
-    <h3>Main Branch</h3>
-    <p>This app was created using a clean bootstrapped react template.</p>
-    <p>The stock react App.js code has been removed.</p>
+      <BasicTabs />
     </div>
   );
 }
